549a6fbf3350cf96ffadb398ae6266c67b2f60e6
Shocker55 <your.email@example.com>
2022-11-30 05:43:49 +0000

    static_page 1st

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/home.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "GET /home" do
+    it "h1 has SapleApp" do
+      expect(page).to have_selector("h1", text: "Sample APP")
+    end
+  end
 end
