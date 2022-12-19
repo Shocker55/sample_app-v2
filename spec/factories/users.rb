6e756f3f0264327738fbbb76d30684e1b89f3c92
Shocker55 <your.email@example.com>
2022-12-19 09:06:03 +0000

    add admin and delete

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,31 +5,31 @@ FactoryBot.define do
     sequence(:email) { |n| "test#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
+    # admin { true }
   end
 end
 
 FactoryBot.define do
   # factoryの名前をモデル名とは別にしたい場合は、以下のようにクラスを指定する
-  factory :archer, class: "User" do
+  factory :archer, class: User do
     name { "Sterling Archer" }
     sequence(:email) { |n| "test#{n}@example.gov" }
     password { "foobar" }
     password_confirmation { "foobar" }
   end
 
-  # factory :continuous_users, class: User do
-  #   sequence(:name) { |n| "User #{n}" }
-  #   sequence(:email) { |n| "user-#{n}@example.com" }
-  #   password { 'password' }
-  #   password_confirmation { 'password' }
-  # end
-end
-
-FactoryBot.define do
   factory :continuous_users, class: User do
     sequence(:name) { |n| "User #{n}" }
     sequence(:email) { |n| "user-#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+  end
+
+  factory :admin, class: User do
+    name  { "admin" }
+    sequence(:email) { |n| "admin#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+    admin { true }
   end
 end
