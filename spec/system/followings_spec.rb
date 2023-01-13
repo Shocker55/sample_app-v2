ea5e09676422c521339bc593368fe3cd9dc63f8d
Shocker55 <your.email@example.com>
2023-01-13 03:28:17 +0000

    add Follow function

---
+++ spec/system/followings_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Followings", type: :system do
+  before do
+    driven_by(:rack_test)
+    @user = FactoryBot.send(:create_relationships)
+    log_in_as @user
+  end
+
+  describe "following page" do
+    it "has following number and a link to following user" do
+      visit following_user_path(@user)
+      expect(@user.following).to_not be_empty
+      expect(page).to have_content "10 following"
+      @user.following.each do |user|
+        expect(page).to have_link user.name, href: user_path(user)
+      end
+    end
+  end
+
+  describe "followers page" do
+    it "has followers number and a link to followers user" do
+      visit followers_user_path(@user)
+      expect(@user.followers).to_not be_empty
+      expect(page).to have_content "10 following"
+      @user.followers.each do |user|
+        expect(page).to have_link user.name, href: user_path(user)
+      end
+    end
+  end
+end
